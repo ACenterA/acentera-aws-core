cc42f1be1e21c7b17172ca64cd1de00fd992aa73
Francis Lavalliere <Francis.Lavalliere@acentera.com>
2019-05-21 17:52:31 -0400

    fix env local / remte

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,9 +1,10 @@
 const API_HOST = process.env.API_HOST || '127.0.0.1';
 const DEV_ACCOUNT_ID = process.env.DEV_ACCOUNTID || '';
+const ENV_CONFIG = process.env.ENV_CONFIG || 'remote';
 
 module.exports = {
   NODE_ENV: '"development"',
-  ENV_CONFIG: '"local"',
+  ENV_CONFIG: `"${ENV_CONFIG}"`,
   BASE_API: `"http://${API_HOST}:2000/api/"`,
   BASE_PLUGIN_URL: `"http://${API_HOST}:2000"`,
   GRAPHQL_LOCAL_URL: `"http://${API_HOST}:4000"`,
